b9b00746c60498488b4523c2b7cdcd4d9c0da15d
Administrator <daedov@gmail.com>
2024-06-21 18:40:34 -0400

    ci: add a condition to create zip file

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
 AWS_REGION="us-east-1"
-LAMBDA_FUNCTION_NAME="my-lambda-function"
+LAMBDA_FUNCTION_NAME="lambda-function"
 LAMBDA_HANDLER="main.lambda_function"
 LAMBDA_ROLE_ARN="arn:aws:iam::992382788926:role/jenkins"  
 ZIP_FILE="lambda_function.zip"  
 
-aws lambda create-function \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --runtime python3.10 \
-    --role $LAMBDA_ROLE_ARN \
-    --handler $LAMBDA_HANDLER \
-    --zip-file fileb://$ZIP_FILE \
-    --region $AWS_REGION \
-    --publish
+if [ ! -f "$ZIP_FILE" ]; then
+    echo "Error: No se encontró el archivo $ZIP_FILE"
+    exit 1
+fi
 
-aws lambda publish-version \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --region $AWS_REGION
+aws lambda get-function --function-name $LAMBDA_FUNCTION_NAME --region $AWS_REGION 2>/dev/null
+if [ $? -ne 0 ]; then
+    aws lambda create-function \
+        --function-name $LAMBDA_FUNCTION_NAME \
+        --runtime python3.8 \
+        --role $LAMBDA_ROLE_ARN \
+        --handler $LAMBDA_HANDLER \
+        --zip-file fileb://$ZIP_FILE \
+        --region $AWS_REGION \
+        --publish
+else
+    aws lambda update-function-code \
+        --function-name $LAMBDA_FUNCTION_NAME \
+        --zip-file fileb://$ZIP_FILE \
+        --region $AWS_REGION \
+        --publish
+fi
+
+VERSION=$(aws lambda publish-version --function-name $LAMBDA_FUNCTION_NAME --region $AWS_REGION --query 'Version' --output text)
 
 aws lambda update-alias \
     --function-name $LAMBDA_FUNCTION_NAME \
     --name PROD \
-    --function-version \$LATEST \
+    --function-version $VERSION \
     --region $AWS_REGION
+
