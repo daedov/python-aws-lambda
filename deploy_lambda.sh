1f1d48567f7b50288cf07f876ac765238901a26b
Administrator <daedov@gmail.com>
2024-06-22 14:58:55 -0400

    ci: update script and pipeline for testing

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -8,21 +8,46 @@ ZIP_FILE="lambda_function.zip"
 
 zip -r function.zip .
 
+wait_until_active() {
+    local status=""
+    while [ "$status" != "Active" ]; do
+        status=$(aws lambda get-function-configuration --function-name $LAMBDA_FUNCTION_NAME --region $AWS_REGION --query 'State' --output text)
+        if [ "$status" == "Failed" ]; then
+            echo "Error: La creación o actualización de la función Lambda falló."
+            exit 1
+        fi
+        echo "Esperando a que la función Lambda esté en estado 'Active'... Estado actual: $status"
+        sleep 10
+    done
+}
 
-aws lambda create-function \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --runtime python3.8 \
-    --role $LAMBDA_ROLE_ARN \
-    --handler $LAMBDA_HANDLER \
-    --zip-file fileb://$ZIP_FILE \
-    --region $AWS_REGION \
-    --publish
-
-aws lambda update-function-code \
-    --function-name $LAMBDA_FUNCTION_NAME \
-    --zip-file fileb://$ZIP_FILE \
-    --region $AWS_REGION \
-    --publish
+aws lambda get-function --function-name $LAMBDA_FUNCTION_NAME --region $AWS_REGION 2>/dev/null
+if [ $? -ne 0 ]; then
+    aws lambda create-function \
+        --function-name $LAMBDA_FUNCTION_NAME \
+        --runtime python3.8 \
+        --role $LAMBDA_ROLE_ARN \
+        --handler $LAMBDA_HANDLER \
+        --zip-file fileb://$ZIP_FILE \
+        --region $AWS_REGION \
+        --publish
+    wait_until_active 
+else
+    wait_until_active  
+    aws lambda update-function-code \
+        --function-name $LAMBDA_FUNCTION_NAME \
+        --zip-file fileb://$ZIP_FILE \
+        --region $AWS_REGION \
+        --publish
+    wait_until_active  
+fi
 
 VERSION=$(aws lambda publish-version --function-name $LAMBDA_FUNCTION_NAME --region $AWS_REGION --query 'Version' --output text)
 
+aws lambda update-alias \
+    --function-name $LAMBDA_FUNCTION_NAME \
+    --name PROD \
+    --function-version $VERSION \
+    --region $AWS_REGION
+
+echo "La función Lambda se ha desplegado y la nueva versión se ha publicado exitosamente."
